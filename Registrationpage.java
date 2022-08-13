b65c19aa0cbb7042175b898b7b35b05b72a19b70
MD <mmislamqa@gmail.com>
2022-08-13 17:31:50 -0400

    my task completed

--- Registrationpage.java
+++ Registrationpage.java
@@ -1,2 +1,9 @@
 this is registration page
+<<<<<<< Updated upstream
 my manager work
+=======
+1st day java lines of code completed
+2nd day java lines of conde completed
+3rd day java lines of code completed
+4th day java lines of code completed
+5th day java lines of code completed
